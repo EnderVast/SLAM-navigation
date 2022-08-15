d94d5787ae365ce5cc51cba103715e7222c488cd
yangfoo <62204580+yangfoo@users.noreply.github.com>
2022-08-15 15:05:53 +0800

    Calibration week 3 stuff

--- milestone2/calibration/wheel_calibration.py
+++ milestone2/calibration/wheel_calibration.py
@@ -43,7 +43,10 @@ def calibrateWheelRadius():
     num = len(wheel_velocities_range)
     scale = 0
     for delta_time, wheel_vel in zip(delta_times, wheel_velocities_range):
-        pass # TODO: replace with your code to compute the scale parameter using wheel_vel and delta_time
+        # pass # TODO: replace with your code to compute the scale parameter using wheel_vel and delta_time
+        tick = wheel_vel * delta_time
+        scale += 1/tick
+    scale /= num
     print("The scale parameter is estimated as {:.6f} m/ticks.".format(scale))
 
     return scale
@@ -86,7 +89,9 @@ def calibrateBaseline(scale):
     num = len(wheel_velocities_range)
     baseline = 0
     for delta_time, wheel_vel in zip(delta_times, wheel_velocities_range):
-        pass # TODO: replace with your code to compute the baseline parameter using scale, wheel_vel, and delta_time
+        # pass # TODO: replace with your code to compute the baseline parameter using scale, wheel_vel, and delta_time
+        baseline += scale * delta_time * wheel_vel / math.pi
+    baseline /= num
     print("The baseline parameter is estimated as {:.6f} m.".format(baseline))
 
     return baseline
